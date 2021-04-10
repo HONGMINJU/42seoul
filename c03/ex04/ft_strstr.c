07f586a221037f560351821810e91ea833578da5
HONGMINJU <mhong0800@gmail.com>
2021-04-10 20:49:46 +0900

    handle merge confilct

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,17 +6,12 @@
 /*   By: mhong <mhong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 18:28:32 by mhong             #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/04/10 20:43:01 by mhong            ###   ########.fr       */
-=======
-/*   Updated: 2021/04/08 13:53:45 by mhong            ###   ########.fr       */
->>>>>>> c42d4c372a94d191a73f6127fa864e7674781dfb
+/*   Updated: 2021/04/10 20:49:16 by mhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
-<<<<<<< HEAD
 	int i;
 	int j;
 
@@ -24,11 +19,6 @@ char	*ft_strstr(char *str, char *to_find)
 	if (!to_find[0])
 		return (str);
 	while (str[i])
-=======
-	if (!*to_find)
-		return (str);
-	while (*str)
->>>>>>> c42d4c372a94d191a73f6127fa864e7674781dfb
 	{
 		j = 0;
 		while (str[i + j] && str[i + j] == to_find[j])
