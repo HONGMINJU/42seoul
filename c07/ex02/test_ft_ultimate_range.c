6b31c71b55c5a73aabc66b5d969fc0b764f172bb
HONGMINJU <mhong0800@gmail.com>
2021-04-11 23:35:08 +0900

    c07(~ex03)

--- c07/ex02/test_ft_ultimate_range.c
+++ c07/ex02/test_ft_ultimate_range.c
@@ -6,13 +6,14 @@
 /*   By: mhong <mhong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/11 15:52:40 by mhong             #+#    #+#             */
-/*   Updated: 2021/04/11 22:27:53 by mhong            ###   ########.fr       */
+/*   Updated: 2021/04/11 22:26:43 by mhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
-int	ft_ultimate_range(int **range, int min, int max)
+int ft_ultimate_range(int **range, int min, int max)
 {
 	long long res;
 	int idx;
@@ -24,10 +25,23 @@ int	ft_ultimate_range(int **range, int min, int max)
 		*range = NULL;
 		return (0);
 	}
-	if (!(ptr = (int *)malloc(sizeof(int) * (max - min))))
+	if(!(ptr = (int *)malloc(sizeof(int) * (max - min))))
 		return (-1);
 	while (min < max)
 		ptr[idx++] = min++;
 	*range = ptr;
 	return ((int)res);
+}
+
+ 
+int main()
+{
+	int* nums;
+	printf("result : %d\n",ft_ultimate_range(&nums, 3, 8));
+	int idx = 0;
+	while(idx < 5)
+	{
+		printf("%d ",nums[idx]);
+		idx++;
+	}
 }
---
+++ c07/ex03/ft_strjoin.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhong <mhong@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/11 22:28:25 by mhong             #+#    #+#             */
+/*   Updated: 2021/04/11 23:26:03 by mhong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char			*ft_strcat(char *str1, char *str2)
+{
+	while (*str2)
+		*str1++ = *str2++;
+	*str1 = '\0';
+	return (str1);
+}
+
+int				ft_strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+unsigned int	calculate_total_len(int size, char **strs, char *sep)
+{
+	int			idx;
+	unsigned	total_len;
+
+	total_len = 0;
+	idx = 0;
+	while (idx < size)
+		total_len += ft_strlen(strs[idx++]);
+	total_len += ((size - 1) * ft_strlen(sep));
+	return (total_len);
+}
+
+char			*ft_strjoin(int size, char **strs, char *sep)
+{
+	unsigned int	total_len;
+	int				idx;
+	char			*result;
+	char			*ptr;
+
+	if (size == 0)
+	{
+		result = (char *)malloc(sizeof(char) * 1);
+		*result = '\0';
+		return (result);
+	}
+	total_len = calculate_total_len(size, strs, sep);
+	if (!(result = (char *)malloc(sizeof(char) * (total_len + 1))))
+		return (NULL);
+	idx = 0;
+	ptr = result;
+	result[0] = '\0';
+	result = ft_strcat(result, strs[idx++]);
+	while (idx < size)
+	{
+		result = ft_strcat(result, sep);
+		result = ft_strcat(result, strs[idx++]);
+	}
+	*result = '\0';
+	return (ptr);
+}
